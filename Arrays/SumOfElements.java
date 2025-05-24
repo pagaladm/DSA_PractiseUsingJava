96cc8d69fcc4804b9c575cc7616f21fc216d6f39
Ananya Priyadarshini <kiss24859@gmail.com>
2025-05-24 06:16:10 +0530

    fourth
    sum of elements

---
+++ Arrays/SumOfElements.java
@@ -0,0 +1,28 @@
+package Arrays;
+import java.util.*;
+public class SumOfElements {
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+        int[] arr={10,20,30,40,50};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+
+        // sum of elements
+        int sum=0;
+        for (int i = 0; i < arr.length; i++) {
+            sum=sum+arr[i];
+        }
+
+        // output
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+
+
+
+    sc.close();
+    }
+}
