d142c869788f0a04e84318d45bdef648b78e02e5
Ananya Priyadarshini <kiss24859@gmail.com>
2025-05-24 06:21:58 +0530

    fourth
    display the sum not the array

--- Arrays/SumOfElements.java
+++ Arrays/SumOfElements.java
@@ -12,7 +12,7 @@ public class SumOfElements {
         // sum of elements
         int sum=0;
         for (int i = 0; i < arr.length; i++) {
-            sum=sum+arr[i];
+            sum+=arr[i];
         }
 
         // output
