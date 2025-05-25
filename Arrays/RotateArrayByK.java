fe82ae473bd08da39a104bed2b5897c2572fdda2
Ananya Priyadarshini <kiss24859@gmail.com>
2025-05-25 14:55:40 +0530

    fifth
    rotate the array by k positions using a new arr

---
+++ Arrays/RotateArrayByK.java
@@ -0,0 +1,33 @@
+package Arrays;
+import java.util.*;
+public class RotateArrayByK {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+            int[] arr={10,20,30,40,50};
+            // input
+            System.out.print("Input Array: ");
+            for (int i = 0; i < arr.length; i++) {
+                System.out.print(arr[i]+ " ");
+            }
+            System.out.println();
+            // rotate the array by k positions
+            System.out.println("Positions to rotate: ");
+            int k=sc.nextInt();
+
+            int n=arr.length;
+            int[] arr2=new int[n];
+
+            for (int i = k; i < n; i++) {
+                arr2[i]=arr[k];
+                k++;
+            }
+            System.out.print("Output Array: ");
+            for (int i = 0; i < arr2.length; i++) {
+                System.out.print(arr2[i]+ " ");
+            }
+            System.out.println();
+
+
+        sc.close();
+    }
+}
